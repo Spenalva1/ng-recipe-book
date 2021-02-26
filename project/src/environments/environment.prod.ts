0e6658baffcae569aae5d547a431d5b27e1f0005
spenalva1 <penalvasantiagogm@gmail.com>
2021-02-26 18:18:23 -0300

    app deployed

--- project/src/environments/environment.prod.ts
+++ project/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  recipesEndpoint: 'https://ng-recipe-book-7cdf7-default-rtdb.firebaseio.com/recipes.json',
+  apiKey: 'AIzaSyBDmaqYfF7eSoW7Jx3LxK_Q5hIl5EAFTk8'
 };
